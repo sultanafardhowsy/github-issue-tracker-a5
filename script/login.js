fc94c8ee2c336fa793ca843f7830d14eca2768df
sultanafardhowsy <sfardhowsy@gmail.com>
2026-03-07 19:56:25 +0600

    login form connected

---
+++ script/login.js
@@ -0,0 +1,16 @@
+
+document.getElementById('login-btn').addEventListener('click',function(){
+const inputNumber = document.getElementById("user-name");
+const number = inputNumber.value ;
+console.log(number);
+
+const inputPin = document.getElementById("password-pin");
+const pin = inputPin.value;
+console.log(pin);
+if( number == 'admin' && pin == 'admin123'){
+    alert('success');
+    window.location.assign("./github-issue.html")
+}
+else alert("login failed");
+
+})
